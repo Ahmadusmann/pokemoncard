235103a65b0c34d984e3b83042cb1b720f37e72d
Wela coder <ahmaddogar818@gmail.com>
2025-11-03 14:41:19 +0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+// import { ToggleSwitch } from "./toggle/toggle_btn.jsx";
+//? Pokemon Project
+import { Pokemon } from "./Pokemon_cards/Pokemon.jsx";
+
+export const App = () => {
+  return (<section className="container">
+    {/* < ToggleSwitch /> */}
+
+   {/* //? Pokemon Project */}
+   <Pokemon />
+    </section>
+  );
+};
